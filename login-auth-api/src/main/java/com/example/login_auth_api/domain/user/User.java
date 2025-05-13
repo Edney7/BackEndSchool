02386659a09c165029a609fea8578a9f708c4097
Edney Araujo <161326470+Edney7@users.noreply.github.com>
2025-05-13 16:29:50 -0300

    Add files via upload

---
+++ login-auth-api/src/main/java/com/example/login_auth_api/domain/user/User.java
@@ -0,0 +1,33 @@
+package com.example.login_auth_api.domain.user;
+
+import java.sql.Date;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.UUID)
+	private Long id;
+	private String name;
+	private String email;
+	private String password;
+	private String sexo;
+	private Integer cpf;
+	private Date date;
+}
